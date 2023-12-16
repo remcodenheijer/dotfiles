0fddc26e4e20a599a1e1c8bb267d3e045d2bb332
Remco den Heijer <remcodenheijer@gmail.com>
2023-12-16 16:10:42 +0100

    Update

--- zsh/scripts.sh
+++ zsh/scripts.sh
@@ -11,3 +11,11 @@ lnks() {
 btht() {
     bluetoothctl connect 0C:AE:BD:1C:77:32
 }
+
+# fe [FUZZY PATTERN] - Open the selected file with the default editor
+#   - Bypass fuzzy finder if there's only one match (--select-1)
+#   - Exit if there's no match (--exit-0)
+fe() {
+  IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
+  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+}
