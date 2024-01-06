cd2d81cc89b10cd25546f2785ba21cfd5ffe8611
Remco den Heijer <remcodenheijer@gmail.com>
2024-01-06 21:13:44 +0100

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -218,6 +218,12 @@ require('lazy').setup({
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   -- { import = 'custom.plugins' },
+  --
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^3', -- Recommended
+    ft = { 'rust' },
+  }
 }, {})
 
 -- [[ Setting options ]]
